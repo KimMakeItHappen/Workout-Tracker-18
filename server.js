072e4febbccd0dc155c6eaa897a992ca43f6d59d
Kimberly Perez <kperez22692@gmail.com>
2021-10-24 23:37:40 -0400

    update

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const logger = require('morgan');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
-const PORT = process.env.PORT || 3001;
-const routes = require('./routes');
+
+const PORT = 3000;
 
 const app = express();
-app.use(morgan('dev'));
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost/workout', {
@@ -16,8 +19,10 @@ mongoose.connect('mongodb://localhost/workout', {
   useUnifiedTopology: true,
 });
 
-app.use(routes);
+// routes
+app.use(require('./routes/api.js'));
+app.use(require('./routes/view.js'));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
